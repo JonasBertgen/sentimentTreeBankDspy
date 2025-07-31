b7b5feecc55101399f32aa896c469a517b1a3618
JonasBertgen <jonas.bertgen@gmail.com>
2025-07-31 14:22:29 +0200

    Moved prediction in own function

--- classifyer.py
+++ classifyer.py
@@ -4,26 +4,28 @@ import time
 
 
 def main():
-    simpleModelName = "llama3.2"
-    simpleLLM = dspy.LM(f"ollama_chat/{simpleModelName}",
-                        api_base="http://localhost:11434", api_key="")
-    dspy.configure(lm=simpleLLM)
+    dictionary = {"verylow": 0, "low": 1,
+                  "neutral": 2, "high": 3, "veryhigh": 4}
+    model = OllamaModel("llama3.2", "test")
+
+
+def makePredictions(model, dataset):
+    dspy.configure(lm=model.model)
     movieReviewSentimentClassifier = dspy.Predict(ReviewClasifier)
-    dataSet = loadDataSet("data/test.txt")
+    dataSet = loadDataSet("data/{dataset}.txt")
     predictions = []
     startTime = time.time()
-    count = 0
+
     for review in dataSet:
-        count += 1
-        predition = f"{count} {review} ({movieReviewSentimentClassifier(
+        predition = f"{review} ({movieReviewSentimentClassifier(
             movieReview=review).get("sentiment")})"
-        print(predition)
         predictions.append(predition)
     endTime = time.time()
 
     with open(r"predictions/llama3.2(vanilla)", "w") as f:
-        f.writelines(predictions)
-    print(f"Total time {endTime - startTime} for model {simpleModelName}")
+        f.writelines("\n".join(predictions))
+    print(f"Total time {endTime - startTime} for model {model.name}")
+    return
 
 
 def loadDataSet(path):
@@ -39,6 +41,13 @@ class ReviewClasifier(dspy.Signature):
     movieReview: str = dspy.InputField()
     sentiment: Literal["verylow", "low", "neutral",
                        "high", "veryhigh"] = dspy.OutputField()
+
+
+class OllamaModel():
+    def __init__(self, modelName):
+        self.name = modelName
+        self.model = dspy.LM(f"ollama_chat/{modelName}",
+                             api_base="http://localhost:11434", api_key="")
 
 
 if __name__ == "__main__":
